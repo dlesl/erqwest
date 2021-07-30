1bacaa929c2fe907610ad72992b2c92181ba71f9
David <dlesl@users.noreply.github.com>
2021-07-30 11:49:50 +0200

    mac fixes

--- shell.nix
+++ shell.nix
@@ -1,14 +1,18 @@
 { pkgs ? import ./pkgs.nix { } }:
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    bashInteractive
-    cargo
-    rust-analyzer
-    rustc
-    rustfmt
-    openssl
-    pkgconfig
-    erlang
-    rebar3
-  ];
+  buildInputs = with pkgs;
+    ([
+      bashInteractive
+      cargo
+      rust-analyzer
+      rustc
+      rustfmt
+      openssl
+      pkgconfig
+      erlang
+      rebar3
+    ] ++ lib.optionals stdenv.isDarwin [
+      libiconv
+      darwin.apple_sdk.frameworks.Security
+    ]);
 }
