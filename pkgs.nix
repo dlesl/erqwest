74826096d1da95fb3abbdc1f579c2126674dc503
David <dlesl@users.noreply.github.com>
2021-08-05 20:22:42 +0200

    update nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3669b12f35aa4cce673191e061349e70bb3d4ddd.tar.gz")
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06552b72346632b6943c8032e57e702ea12413bf.tar.gz")
