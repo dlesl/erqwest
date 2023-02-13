d99afaead9b063485ae0ab2ff2bd15f58581a3ff
David <dlesl@users.noreply.github.com>
2023-02-13 02:50:15 +0100

    bump deps

--- pkgs.nix
+++ pkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/05232d19b68ff95c23571f782b552eaf71e0a742.tar.gz")
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/80069cc798f5359be1635c0a4650e7dba1b8fdc9.tar.gz")
